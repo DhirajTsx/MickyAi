d8f98b66794d3fa2eb80226304d4805539550055
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2026-09-07 17:05:01 +0530

    Initial Commit

---
+++ MackyAi/MackyAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MackyAiApp.swift
+//  MackyAi
+//
+//  Created by Dhiraj Bhawsar on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MackyAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
